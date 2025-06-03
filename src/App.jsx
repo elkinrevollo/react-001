7e909a9d3df5464ae5480cd68d509c3ced948d5f
elkinrevollo <elkinrevollo.sis24@tecba.edu.bo>
2025-06-03 10:04:31 -0400

    empezando con react parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
     <>
       <h1>npm create vite@latest</h1>
       <h2>Elkin Revollo Rojas</h2>
+      <h2>aprendiendo react
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia sapiente eum et. Laudantium dolores nemo neque, similique atque, quos sed soluta non minima sit velit doloremque, et pariatur expedita possimus.
+      Sapiente nihil asperiores porro molestias quibusdam eaque dolor in iste! Soluta voluptatibus totam placeat, dicta blanditiis aliquam necessitatibus nihil nobis distinctio aspernatur inventore architecto nisi voluptate explicabo et excepturi eaque?</p>
+      </h2>
     </>
   )
 }
